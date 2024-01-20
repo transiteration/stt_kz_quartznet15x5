7943bf4844cae03ceda7eba4c9725aae36e6f81b
transiteration <78851937+transiteration@users.noreply.github.com>
2024-01-20 19:18:08 +0900

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -24,5 +24,5 @@ if __name__ == "__main__":
 
     predict_model(
         model_path=args.model_path, 
-        audio_file_path=args.audio_file_path,             )
+        audio_file_path=args.audio_file_path,
     )
